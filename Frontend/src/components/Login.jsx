8ea23c5bd914345eb78f525a3d5987f17a0f4a5d
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-20 09:44:23 +0530

    Added validations for password field

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -33,17 +33,48 @@ const Login = () => {
 
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" {...register("email")} id="email" />
+          <input
+            type="email"
+            {...register("email", {
+              required: "Email is required",
+              minLength: {
+                value: 5,
+                message: "Email must be at least 5 characters long",
+              },
+            })}
+            id="email"
+          />
         </div>
 
         <div>
           <label htmlFor="mobile">Mobile</label>
-          <input type="number" {...register("mobile")} id="mobile" />
+          <input
+            type="number"
+            {...register("mobile", {
+              required: "Mobile number is required",
+              minLength: {
+                value: 5,
+                message: "Mobile number must be at least 5 characters long",
+              },
+            })}
+            id="mobile"
+          />
         </div>
 
         <div>
           <label htmlFor="password">Passowrd</label>
-          <input id="password" type="password" {...register("password")} />
+          <input
+            id="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password should be at least 8 characters",
+              },
+            })}
+            type="password"
+            {...register("password")}
+          />
         </div>
 
         <button type="submit">Register</button>
