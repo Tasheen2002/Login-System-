7dc898d0c9ec929ffd7aa454aebdef8c8d4537e2
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-20 09:30:18 +0530

    Added validation for name text field

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,24 +1,51 @@
 import React from "react";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitCall = (data) => {
+    console.log(data);
+  };
+
   return (
-    const 
     <div>
-      <form>
+      <form onSubmit={handleSubmit(submitCall)}>
         <h2>Create an Account</h2>
-        <label htmlFor="fullName">Full Name</label>
-        <input type="text" id="fullName" />
-        <br />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <br />
-        <label htmlFor="mobile">Mobile</label>
-        <input type="number" id="mobile" />
-        <br />
-        <label htmlFor="password">Passowrd</label>
-        <input type="password" id="password" />
-        <br />
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            id="fullName"
+            type="text"
+            {...register("fullName", {
+              required: "Name ia required",
+              minLength: {
+                value: 3,
+                message: "Name must be at least 3 characters long",
+              },
+            })}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" {...register("email")} id="email" />
+        </div>
+
+        <div>
+          <label htmlFor="mobile">Mobile</label>
+          <input type="number" {...register("mobile")} id="mobile" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Passowrd</label>
+          <input id="password" type="password" {...register("password")} />
+        </div>
+
         <button type="submit">Register</button>
       </form>
     </div>
